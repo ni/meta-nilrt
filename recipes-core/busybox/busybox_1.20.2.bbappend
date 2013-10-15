25791881d97fb511953ea7961e4f79279557e8ba
Richard Tollerton <rich.tollerton@ni.com>
2013-10-14 19:18:04 -0500

    busybox-mdev: Import NILRT-specific configuration
    Updated mdev.conf and new automount.sh and usb.sh.
    
    mdev.conf has the following salient changes wrt busybox 1_20_stable
    mdev_fat.conf. This list was reconstructed from a diff. I don't have a
    good history of the changes. This list may be incomplete, or may include
    unimportant changes or changes not made by us.
    
    - $MODALIAS=.*: separate case for remove action
    - ttyPS[0-9]*: xilinx xuartps
    - ttyS[0-9]*: group tty instead of uucp
    - sd[a-z].*: use new automount.sh script (see below)
    - mmcblk[0-9]p([0-9]+): Automount support for SD/MMC partitions
    - tun[0-9]*: 666 permissions
    - rtc0: Replaces rule "rtc"; group ni, symlinked to /dev/misc/rtc
    - initctl, kmsg, log: rules
    - fpgaperipheral, niwatchdog: rules for CompactRIO FPGA/watchdog devices
    - all usb devices usbdev.* now use usb.sh script (see below)
    - mtd[2-3]: Partitions for uboot environment variables; need to be
      readable by all users
    - video([0-9]+): World-writable; owned by group ni
    - rfkill: New rule, owned by group ni
    
    automount.sh is an automount script for USB and SD devices. Adds/removes
    symlinks to /media/<devname>. Also creates compatibility symlinks to /u,
    /U, /v, /V, ... for compatibility with LabVIEW RT VIs that still employ
    DOS-style drive letters.
    
    usb.sh manages uevent device nodes under /dev/bus/usb.
    
    Natinst-P4-Changelist-ID: 8111313
    Natinst-ReviewBoard-ID: 44238
    
    Signed-off-by: Richard Tollerton <rich.tollerton@ni.com>

--- recipes-core/busybox/busybox_1.20.2.bbappend
+++ recipes-core/busybox/busybox_1.20.2.bbappend
@@ -1,11 +1,13 @@
 
 FILESEXTRAPATHS_prepend := "${THISDIR}:${THISDIR}/files:${THISDIR}/${PN}-${PV}:"
 
+SRC_URI =+ "file://automount.sh file://usb.sh"
 SRC_URI =+ "file://busybox-ifplugd"
 
 PACKAGES =+ " ${PN}-cron"
 PACKAGES =+ " ${PN}-ifplugd"
 
+FILES_${PN}-mdev += "${sysconfdir}/mdev ${sysconfdir}/mdev/automount.sh ${sysconfdir}/mdev/usb.sh "
 FILES_${PN}-ifplugd = "${sysconfdir}/init.d/busybox-ifplugd"
 FILES_${PN}-cron = "${sysconfdir}/init.d/busybox-cron ${sysconfdir}/cron/crontabs"
 
@@ -23,5 +25,12 @@ do_install_append () {
 	fi
 	if grep "CONFIG_IFPLUGD=y" ${B}/.config; then
 	       install -m 0755 ${WORKDIR}/busybox-ifplugd ${D}${sysconfdir}/init.d/
+	fi
+        if grep "CONFIG_MDEV=y" ${B}/.config; then
+               if grep "CONFIG_FEATURE_MDEV_CONF=y" ${B}/.config; then
+		       install -d ${D}${sysconfdir}/mdev
+		       install -m 0755 ${WORKDIR}/automount.sh ${D}${sysconfdir}/mdev/automount.sh
+		       install -m 0755 ${WORKDIR}/usb.sh ${D}${sysconfdir}/mdev/usb.sh
+               fi
 	fi
 }
---
+++ recipes-core/busybox/files/automount.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+mountdir="/media"
+
+symlink_cleanup()
+{
+	local symlink_target
+
+	# clear dangling compatibility symlinks
+	for symlink in u v w x y z U V W X Y Z
+	do
+		if [ -h "/$symlink" ]; then
+			symlink_target=`readlink /$symlink`
+			if [ ! -e "$symlink_target" ]; then
+				rm "/$symlink"
+			else
+				grep -q -F "$symlink_target" /proc/mounts \
+					|| rm "/$symlink"
+			fi
+		fi
+	done
+}
+
+add_action()
+{
+	# check if the device exists; otherwise exit
+	[ ! -b "${MDEV}" ] && exit 1
+
+	symlink_cleanup
+
+	mkdir -p "${mountdir}/$1" || exit 1
+
+	if ! mount -t auto "/dev/$1" "${mountdir}/$1"; then
+		rmdir "${mountdir}/$1"
+		exit 1
+	fi
+
+	# create compatibility symlinks
+	for symlink in U V W X Y Z
+	do
+		symlink_lowercase=$(echo $symlink | tr '[:upper:]' '[:lower:]')
+		if [ ! -e "/$symlink" -a ! -e "/$symlink_lowercase" ]; then
+			ln -s "${mountdir}/$1" "/$symlink"
+			ln -s "${mountdir}/$1" "/$symlink_lowercase"
+			break
+		fi
+	done
+}
+
+remove_action()
+{
+	if grep -qs "^/dev/$1 " /proc/mounts ; then
+		umount "${mountdir}/$1";
+	fi
+
+	[ -d "${mountdir}/$1" ] && rmdir "${mountdir}/$1"
+
+	symlink_cleanup
+}
+
+case "${ACTION}" in
+	add|"")
+		add_action "${MDEV}"
+		;;
+	remove)
+		remove_action "${MDEV}"
+		;;
+esac
