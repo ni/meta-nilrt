699338759744ebe898f861be09a264214bd01888
Bill Pittman <bill.pittman@ni.com>
2014-02-04 13:11:06 -0600

    udev-extraconf: implement compatibility symlinks for LabVIEW RT
    create/remove compatibility symlinks (ie /u /v ...) for VIs that
    use DOS style drive lettering for removable media.
    
    Natinst-Rally-ID: TA46865
    Natinst-ReviewBoard-ID: 58266
    
    Signed-off-by: Bill Pittman <bill.pittman@ni.com>

--- recipes-core/udev/udev-extraconf/mount.sh
+++ recipes-core/udev/udev-extraconf/mount.sh
@@ -21,7 +21,26 @@ do
 	fi
 done
 
-automount() {	
+symlink_cleanup()
+{
+        local symlink_target
+
+        # clear dangling compatibility symlinks
+        for symlink in u v w x y z U V W X Y Z
+        do
+                if [ -h "/$symlink" ]; then
+                        symlink_target=`readlink /$symlink`
+                        if [ ! -e "$symlink_target" ]; then
+                                rm "/$symlink"
+                        else
+                                grep -q -F "$symlink_target" /proc/mounts \
+                                        || rm "/$symlink"
+                        fi
+                fi
+        done
+}
+
+automount() {
 	name="`basename "$DEVNAME"`"
 
 	! test -d "/media/$name" && mkdir -p "/media/$name"
@@ -38,6 +57,17 @@ automount() {
 	else
 		logger "mount.sh/automount" "Auto-mount of [/media/$name] successful"
 		touch "/tmp/.automount-$name"
+
+		# create compatibility symlink
+		for symlink in U V W X Y Z
+		do
+			symlink_lowercase=$(echo $symlink | tr '[:upper:]' '[:lower:]')
+			if [ ! -e "/$symlink" -a ! -e "/$symlink_lowercase" ]; then
+				ln -s "/media/$name" "/$symlink"
+				ln -s "/media/$name" "/$symlink_lowercase"
+				break
+			fi
+		done
 	fi
 }
 	
@@ -45,7 +75,7 @@ rm_dir() {
 	# We do not want to rm -r populated directories
 	if test "`find "$1" | wc -l | tr -d " "`" -lt 2 -a -d "$1"
 	then
-		! test -z "$1" && rm -r "$1"
+		! test -z "$1" && rm -r "$1" && symlink_cleanup
 	else
 		logger "mount.sh/automount" "Not removing non-empty directory [$1]"
 	fi
@@ -57,6 +87,8 @@ if [ "$ACTION" = "add" ] && [ -n "$DEVNAME" ] && [ -n "$ID_FS_TYPE" ]; then
 	elif [ -x $MOUNT ]; then
     		$MOUNT $DEVNAME 2> /dev/null
 	fi
+
+	symlink_cleanup
 
 	# check if sgdisk is available, if not, ignore the EFI check
 	if command -v sgdisk > /dev/null; then
