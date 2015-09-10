507b42014baa6b561e5e07350d341ba92a140493
Joshua Hernstrom <joshua.hernstrom@ni.com>
2015-09-10 09:44:48 -0500

    initscripts: Filter characters used in generated hostname
    If a product includes 'Embedded Controller' in the name, strip
    that from the default hostname. Also, VMWare images return serial#
    values with spaces and with a length greater than necessary for
    our initial autogenerated value, so limit the characters used to
    the last eight usable values for consistency with serial numbers
    of NI controllers.
    
    Signed-off-by: Joshua Hernstrom <joshua.hernstrom@ni.com>
    Natinst-Reviewboard-ID: 110320

--- recipes-core/initscripts/initscripts-1.0/hostname.sh
+++ recipes-core/initscripts/initscripts-1.0/hostname.sh
@@ -6,29 +6,19 @@
 #
 
 # Check if there is a hostname in ni-rt.ini
-HOSTNAME=`/usr/local/natinst/bin/nirtcfg --get section=SystemSettings,token=Host_Name`
+HOSTNAME=$(/usr/local/natinst/bin/nirtcfg --get section=SystemSettings,token=Host_Name)
 if [ $? -eq 0 ] && [ -n "$HOSTNAME" ]; then
     # Set the hostname based on ni-rt.ini
     hostname "$HOSTNAME"
 else
     # Generate a default hostname based on the serial# or MAC address
+    MODEL=$((/sbin/fw_printenv -n DeviceDesc 2> /dev/null || echo PXI-Unknown) | sed "s/^NI //;s/ Embedded Controller//")
 
-    MODEL=`(/sbin/fw_printenv -n DeviceDesc 2> /dev/null || echo cRIO-Linux) | sed "s/^NI //" `
+    # Only use the last 8 characters of the serial number (after spaces are stripped)
+    SERIAL=$(/sbin/fw_printenv -n serial# 2>&1 | tr -d "\n " | tail -c 8)
 
-    SERIAL=`/sbin/fw_printenv -n serial# 2>&1`
-    # On success "fw_printenv -n serial# 2>&1" puts 0 in "$?"
-    if test $? -ne 0
-    then
-        # If Uboot variable "fw_setenv serial# AABBCCDD" is not set,
-        # use the mac address to generate the name of the target.
-        echo `ifconfig -a |grep -o "HWaddr [^ ]*" |head -n 1 |\
-           sed -r -e "s/HWaddr /NI-$MODEL-/g" -e "s/://g" -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-//'` | xargs hostname
-    else
-        # If Uboot variable is set, then use its value to generate
-        # the hostname.
-        echo "NI-$MODEL-$SERIAL" | sed -r -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-//' | xargs hostname
-    fi
+    echo "NI-$MODEL-$SERIAL" | sed -r -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-//' | xargs hostname
 
     # Set the newly generated hostname in ni-rt.ini
-    /usr/local/natinst/bin/nirtcfg --set section=SystemSettings,token=Host_Name,value="`hostname`"
+    /usr/local/natinst/bin/nirtcfg --set section=SystemSettings,token=Host_Name,value="$(hostname)"
 fi
