bbe27d20bbffb145a0195ff934fb8818724f4657
Haris Okanovic <haris.okanovic@ni.com>
2015-10-16 14:39:02 -0500

    rtctl: Add rtctld daemon
    A daemon to watch for new irqthreads and invoke rtctl to apply the
    system's priority policy.
    
    Signed-off-by: Haris Okanovic <haris.okanovic@ni.com>
    Signed-off-by: Ovidiu-Adrian Vancea <ovidiu.vancea@ni.com>

---
+++ recipes-rt/rtctl/files/rtctl-1.13/rtctld.c
@@ -0,0 +1,197 @@
+/* rtctld
+ * Copyright (C) 2015 National Instruments
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <syslog.h>
+#include <errno.h>
+
+#define BUFSIZE 256
+
+#define PROGRAM_NAME "rtctld"
+
+#define SYSLOG_ERROR(formatStr, ...) \
+    syslog(LOG_DAEMON | LOG_ERR,  ("ERROR: " formatStr), ## __VA_ARGS__)
+
+static const char* const interruptsFilePath = "/proc/interrupts";
+/* Hide standard stdout messages and show stderr as stdout
+ * By design don't redirect both stderr and stdout to null
+ */
+static const char* const configCommand =
+    "/usr/sbin/rtctl reset 2>&1 >/dev/null";
+
+static int doDaemonize = 1;
+static int pollingFd = -1;
+
+static int isRunning = 1;
+
+static void stop_signal_handler(int sig)
+{
+    isRunning = 0;
+}
+
+static void setupSignalHandler(void)
+{
+    struct sigaction act = {0};
+
+    act.sa_handler = &stop_signal_handler;
+
+    if (sigaction(SIGTERM, &act, NULL)) {
+        SYSLOG_ERROR("Failed to register stop signal handler: %s", strerror(errno));
+        exit(errno);
+    }
+}
+
+static void processArgs(const int argc, const char** argv)
+{
+    int i;
+    const char* argi;
+
+    for (i = 1; i < argc; ++i) {
+        argi = argv[i];
+
+        if (strcmp("-x", argi)) {
+            SYSLOG_ERROR("Invalid arg #%d \"%s\"", i, argi);
+            exit(EINVAL);
+        }
+        else {
+            doDaemonize = 0;
+        }
+    }
+}
+
+/*
+ * Fork a child process
+ * Child returns
+ * Parent exits to declare successful initialization
+ */
+static void daemonize(void)
+{
+    pid_t pid = fork();
+    if ((pid_t)-1 == pid) {
+        SYSLOG_ERROR("Failed to fork worker; %s", strerror(errno));
+        exit(errno);
+    }
+
+    if (pid) {
+        exit(0);
+    }
+}
+
+/*
+ * Polls for a change to the file handle
+ * Returns
+ *  >0 on successful event poll
+ *  EINTR on interrupt
+ *  kills process otherwise
+ */
+static int pollChange(void)
+{
+    int res;
+    struct pollfd pfd = {0};
+
+    pfd.fd = pollingFd;
+    pfd.events = POLLPRI | POLLERR;
+
+    res = poll(&pfd, 1, -1);
+    if (res < 0 && EINTR != errno) {
+        SYSLOG_ERROR("Poll failed; %s", strerror(errno));
+        exit(errno);
+    }
+    else if (!res) {
+        SYSLOG_ERROR("Poll timed out, but an infinite timeout was configured");
+        exit(ETIMEDOUT);
+    }
+    /* else, got an event or got interrupted */
+
+    return res;
+}
+
+/* Runs the configure script
+ * Returns exit code of script or EINTR if interrupted
+ * Logs error when config script fails, except interruptions
+ */
+static int runConfig(void)
+{
+    char line[BUFSIZE];
+    int res;
+    FILE *fp = popen(configCommand, "r");
+
+    if (!fp) {
+        SYSLOG_ERROR("Failed to open pipe: %s", strerror(errno));
+        return EIO;
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+        SYSLOG_ERROR("%s", line);
+    }
+
+    res = pclose(fp);
+    if (res) {
+        if (-1 == res) {
+            SYSLOG_ERROR("Call to pclose() failed: %s", strerror(errno));
+        }
+        else if (WIFEXITED(res)) {
+            SYSLOG_ERROR("Config script failed with return value: %d", WEXITSTATUS(res));
+        }
+        else if (WIFSIGNALED(res) &&
+            (WTERMSIG(res) == SIGINT || WTERMSIG(res) == SIGQUIT)) {
+            /* child was interrupted, pass that along */
+            errno = res = EINTR;
+       }
+    }
+
+    return res;
+}
+
+int main(int argc, const char** argv)
+{
+    openlog(PROGRAM_NAME, LOG_PERROR|LOG_CONS|LOG_NDELAY, LOG_DAEMON);
+    setupSignalHandler();
+
+    processArgs(argc, argv);
+
+    pollingFd = open(interruptsFilePath, O_RDONLY);
+    if (pollingFd < 0) {
+        SYSLOG_ERROR("Failed to open polling file \"%s\"; %s",
+            interruptsFilePath, strerror(errno));
+        exit(errno);
+    }
+
+    if (doDaemonize) {
+        daemonize();
+    }
+
+    /* Configure tasks that started before this daemon
+     * All others will be caught in the polling loop
+     */
+    runConfig();
+
+    while (isRunning) {
+        if (pollChange() > 0 && runConfig() != EINTR) {
+            /* just in case */
+            sleep(1);
+        }
+    }
+
+    return 0;
+}
--- recipes-rt/rtctl/rtctl_1.13.bb
+++ recipes-rt/rtctl/rtctl_1.13.bb
@@ -12,7 +12,8 @@ SRC_URI = " \
     http://git.centos.org/sources/rtctl/c7-rt/eb046c6fb02eff54d1907c2b003bbc076eed6ded;downloadfilename=rtctl-${PV}.tar.bz2 \
     file://0001-rtctl-Read-task-IDs-from-procfs-instead-of-ps.patch \
     file://0002-rtctl-Support-multiple-rtgroups-files.patch \
-    file://init \
+    file://rtctl-${PV}/rtctld.c \
+    file://rtctl-${PV}/init.d/rtctld \
 "
 SRC_URI[sha256sum] = "33706ea797f99054049c20d709ca0e5c8ae5daccf347b80e8ac2884266439101"
 SRC_URI[md5sum] = "a530ceb797193c54b0d57a05b1e82d24"
@@ -22,17 +23,23 @@ S = "${WORKDIR}/rtctl-${PV}"
 RDEPENDS_${PN} += "bash"
 
 inherit update-rc.d
-INITSCRIPT_NAME = "rtctl"
-INITSCRIPT_PARAMS = "start 01 5 . stop 00 0 . stop 00 6 ."
+
+INITSCRIPT_NAME = "rtctld"
+INITSCRIPT_PARAMS = "start 99 S . stop 00 0 . stop 00 6 ."
+
+do_compile() {
+    ${CC} -Os ${CFLAGS} rtctld.c -o rtctld ${LDFLAGS}
+}
 
 do_install() {
     install -m 0755 -d ${D}${sbindir}/
-    install -m 0755 -d ${D}${sysconfdir}/init.d/
     install -m 0755 -d ${D}${sysconfdir}/rtgroups.d/
+    install -m 0755 -d ${D}${sysconfdir}/init.d/
 
     install -m 0755 ${S}/rtctl ${D}${sbindir}/
-
-    install -m 0755 ${WORKDIR}/init ${D}${sysconfdir}/init.d/rtctl
+    install -m 0755 ${S}/rtctld ${D}${sbindir}/
 
     install -m 0644 ${S}/rtgroups ${D}${sysconfdir}/
+
+    install -m 0755 ${S}/init.d/rtctld ${D}${sysconfdir}/init.d/
 }
