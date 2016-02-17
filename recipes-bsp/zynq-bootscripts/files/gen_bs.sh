31549c880ea3b0836822a037e1d602787cec8db3
Arnold Csorvasi <arnold.csorvasi@ni.com>
2016-02-17 12:06:49 +0200

    zynq-bootscripts: create bootscripts containing booting logic
    Create 4 script files that contain the booting logic. The gen_bs.sh
    script will create 4 text files, which will be converted to U-Boot
    script files with mkimage.
    The boot.scr file is the top level bootscript. It checks for the
    requested bootmode and loads the appropriate default/safemode/restore.scr
    file. The default.scr contains the commands to boot into the default
    image. The safemode.scr is separated from the default one to handle
    extra requirements for booting into the safemode. The restore.scr
    knows only about the booting logic for restore mode and lives on the
    bootfs partition.

---
+++ recipes-bsp/zynq-bootscripts/files/gen_bs.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+COMMON_BOOTARGS='console=ttyS0,115200 $mtdparts ubi.mtd=boot-config ubi.mtd=root $othbootargs'
+RESTORE_BOOTARGS="setenv bootargs $COMMON_BOOTARGS ramdisk_size=135168 root=/dev/ram rw"
+DEFAULT_BOOTARGS="setenv bootargs $COMMON_BOOTARGS root=ubi1:rootfs rw rootfstype=ubifs threadsirqs=1 kthreadd_pri=25 ksoftirqd_pri=8 irqthread_pri=15 \${usb_gadget_args}"
+
+load_restore_boot()
+{
+    RESTORE_BOOTMODE=$1
+RESTORE_BOOTCMD="
+if ubi part boot-config &&
+    ubifsmount ubi:bootfs &&
+    ubifsload \$loadaddr .restore/restore.scr;
+then
+    restore=$RESTORE_BOOTMODE;
+    source \$loadaddr;
+else
+    echo Restore mode error.;
+fi;
+"
+    echo $RESTORE_BOOTCMD
+}
+
+DEFAULT_BOOTCMD="
+if ubi part root &&
+    ubifsmount ubi:rootfs &&
+    ubifsload \$loadaddr /boot/uImage &&
+    ubifsload \$verifyaddr /boot/ni-\$DeviceCode.dtb;
+then
+    $DEFAULT_BOOTARGS safe_mode=false;
+    bootm \$loadaddr - \$verifyaddr;
+elif
+    ubifsload \$loadaddr /boot/safemode.scr;
+then
+    source \$loadaddr;
+else
+    "`load_restore_boot restore`"
+fi;"
+
+
+SAFEMODE_BOOTCMD="
+if ubi part root &&
+    ubifsmount ubi:rootfs &&
+    ubifsload \$loadaddr /boot/uImage &&
+    ubifsload \$verifyaddr /boot/ni-\$DeviceCode.dtb;
+then
+    $DEFAULT_BOOTARGS safe_mode=true;
+    bootm \$loadaddr - \$verifyaddr;
+else
+    "`load_restore_boot restore`"
+fi;"
+
+RESTORE_BOOTCMD="
+if ubifsload \$loadaddr .restore/uImage &&
+    ubifsload 0x8500000 .restore/ni-\$DeviceCode.dtb &&
+    ubifsload 0x8700000 .restore/ramdisk;
+then
+    $RESTORE_BOOTARGS
+    restore=\$restore;
+    bootm \$loadaddr 0x8700000 0x8500000;
+else
+    echo Restore mode error;
+fi;"
+
+CHECK_SAFEMODE='
+i2c read 0x40 1 1 $verifyaddr;
+setexpr.b cpld_safemode *$verifyaddr \& 0x01;
+if test $cpld_safemode -eq 1;
+then
+    '"$SAFEMODE_BOOTCMD"'
+fi;
+'
+BOOTCMD='
+i2c read 0x40 0x1F 1 $verifyaddr;
+setexpr.b next_bootmode *$verifyaddr \& 0x3;
+if test $next_bootmode -eq 0;
+then
+    if ubifsload $loadaddr /boot/default.scr;
+    then
+        source $loadaddr;
+    elif ubifsload $loadaddr /boot/safemode.scr;
+    then
+        source $loadaddr;
+    else
+        '`load_restore_boot restore`'
+    fi;
+elif test $next_bootmode -eq 2;
+then
+    '`load_restore_boot restore`'
+elif test $next_bootmode -eq 3;
+then
+    '`load_restore_boot auto-restore`'
+else
+    echo FATAL ERROR!;
+fi;
+'
+
+# cRIO-9068 doesn't have USB gadget, so skip it on that device
+USB_GADGET_ARGS='
+if test ${DeviceCode} != 0x76D6;
+then
+    usb_gadget_args="g_ether.idVendor=${USBVendorID} g_ether.idProduct=${USBProductID} g_ether.iProduct=\"${USBProduct} [${hostname}]\" g_ether.iSerialNumber=${serial#} g_ether.dev_addr=${usbgadgetethaddr} g_ether.bcdDevice=${USBDevice}";
+fi;'
+
+echo $USB_GADGET_ARGS > top_level_bootscript
+echo $CHECK_SAFEMODE >> top_level_bootscript
+echo $BOOTCMD >> top_level_bootscript
+echo $DEFAULT_BOOTCMD > default_bootscript
+echo $SAFEMODE_BOOTCMD > safemode_bootscript
+echo $RESTORE_BOOTCMD > restore_bootscript
---
+++ recipes-bsp/zynq-bootscripts/zynq-bootscripts.bb
@@ -0,0 +1,32 @@
+SUMMARY = "Create 4 scr files used by U-Boot to boot into different bootmodes."
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+SRC_URI ="\
+       file://gen_bs.sh \
+           "
+inherit deploy
+
+PR = "r1"
+
+S = "${WORKDIR}"
+
+do_compile() {
+    ${S}/gen_bs.sh
+	mkimage -T script -C none -n 'Top Level Bootscript' -d top_level_bootscript boot.scr
+	mkimage -T script -C none -n 'Default Bootscript' -d default_bootscript default.scr
+	mkimage -T script -C none -n 'Safemode Bootscript' -d safemode_bootscript safemode.scr
+	mkimage -T script -C none -n 'Restore Mode Bootscript' -d restore_bootscript restore.scr
+}
+
+SYSROOT_PREPROCESS_FUNCS += "bootscript_preprocess"
+
+bootscript_preprocess(){
+	install -d ${SYSROOT_DESTDIR}boot
+	install -m 0644 boot.scr ${SYSROOT_DESTDIR}boot/boot.scr
+	install -m 0644 default.scr ${SYSROOT_DESTDIR}boot/default.scr
+	install -m 0644 safemode.scr ${SYSROOT_DESTDIR}boot/safemode.scr
+	install -m 0644 restore.scr ${SYSROOT_DESTDIR}boot/restore.scr
+}
+
+FILES_${PN} = " restore.scr default.src boot.scr safemode.scr "
