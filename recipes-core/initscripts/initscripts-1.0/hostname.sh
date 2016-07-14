30c18c7b7f75b33f0eeaeb95ef3f4d51b3b0b2cb
Collin Richards <collin.richards@ni.com>
2016-07-14 14:47:50 -0500

    hostname.sh: Migrate hostname configuration
    Previosly hostname configuration was done using nirtcfg to modify
    /etc/natinst/share/ni-rt.ini Move configuration to modify /etc/hostname
    file to be more consistant with other distro's - better salt support.
    
    This breaks configuring the hostname using nirtcfg
    
    Signed-off-by: Collin Richards <collin.richards@ni.com>
    Natinst-ReviewBoard-ID: 146050

--- recipes-core/initscripts/initscripts-1.0/hostname.sh
+++ recipes-core/initscripts/initscripts-1.0/hostname.sh
@@ -5,10 +5,13 @@
 # Version:      @(#)hostname.sh  1.10  26-Feb-2001  miquels@cistron.nl
 #
 
-# Check if there is a hostname in ni-rt.ini
-HOSTNAME=$(/usr/local/natinst/bin/nirtcfg --get section=SystemSettings,token=Host_Name)
+if [ -f /etc/hostname ] && [ -r /etc/hostname ];then
+    HOSTNAME=$(cat /etc/hostname)
+else
+    HOSTNAME=
+fi
+
 if [ $? -eq 0 ] && [ -n "$HOSTNAME" ]; then
-    # Set the hostname based on ni-rt.ini
     hostname "$HOSTNAME"
 else
     # Generate a default hostname based on the serial# or MAC address
@@ -19,6 +22,6 @@ else
 
     echo "NI-$MODEL-$SERIAL" | sed -r -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-//' | xargs hostname
 
-    # Set the newly generated hostname in ni-rt.ini
-    /usr/local/natinst/bin/nirtcfg --set section=SystemSettings,token=Host_Name,value="$(hostname)"
+    # Set the newly generated hostname in /etc/hostname
+    hostname > /etc/hostname
 fi
