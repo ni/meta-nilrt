831b4337d75562d039754630eedf256ab2751852
Alex Stewart <alex.stewart@ni.com>
2017-08-15 17:40:39 -0500

    kernel-tests: test_i915_firmware update
    :**/kernel-tests/kernel-tests.bb
    * Add RDEPENDS on dmidecode as it is now used by test_i915_firmware
      to test the system-manufacturer.
    
    :recipes-kernel/**/test_i915_firmware.sh
    * Fix a cornercase in the test that would cause the test to error and
      pass (as opposed to fail) when the i915 module is not loaded.
    * Refactored the test to use the ptest-format utility.
    * Skip the test if we are not on a "National Instruments" manufactured
      system (eg. we are in a VM).
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>
    Acked-by: Ioan-Adrian Ratiu <adrian.ratiu@ni.com>
    Acked-by: Bill Pittman <bill.pittman@ni.com>
    Natinst-ReviewBoard-ID: 197966

--- recipes-kernel/kernel-tests/kernel-tests-files/test_i915_firmware.sh
+++ recipes-kernel/kernel-tests/kernel-tests-files/test_i915_firmware.sh
@@ -1,42 +1,84 @@
 #!/bin/bash
+# This test validates that the i915 driver firmware is installed and that its
+# module is loaded and in use. It is skipped if it is run on an 'arm7l' device.
 
-FAILED=0
+source $(dirname "$0")/ptest-format.sh
 
-ARCH=`uname -m`
-if [ "$ARCH" = "armv7l" ] ; then
-	echo "SKIP: `basename $0`"
-	echo "i915 firmware is not installed on ARM targets"
-	exit 1
-fi
+ptest_test=$(basename "$0" ".sh")  # test_i915_firmware.sh
 
-if [ ! -d /lib/firmware/i915 ] ; then
-	FAILED=1
-fi
-
-SYMS=( "bxt_dmc_ver1.bin" "kbl_dmc_ver1.bin" "skl_dmc_ver1.bin" "skl_guc_ver6.bin" )
-
-for s in "${SYMS[@]}" ; do
-	if [ ! -h /lib/firmware/i915/$s ] ; then
-		FAILED=1
+# Skips this test if we are on an ARM device.
+function check_arch () {
+	ARCH=$(uname -m)
+	if [ "$ARCH" = "armv7l" ] ; then
+		echo "REASON: i915 firmware is not installed on ARM targets"
+		ptest_skip
 	fi
-done
+}
 
-FILES=( "bxt_dmc_ver1_07.bin" "kbl_dmc_ver1_01.bin" "skl_dmc_ver1_26.bin" "skl_guc_ver6_1.bin" )
-
-for f in "${FILES[@]}" ; do
-	if [ ! -f /lib/firmware/i915/$f ] ; then
-		FAILED=1
+# Checks /lib/firmware/i915 and various other files and links. If all exist,
+# this check passes.
+function check_files () {
+	DIR_FIRMWARE='/lib/firmware/i915'
+	if [ ! -d "${DIR_FIRMWARE}" ]; then
+		echo "ERROR: ${DIR_FIRMWARE} not found."
+		ptest_fail
 	fi
-done
 
-MOD=$(lsmod |grep "^i915 " |awk '{print $3}')
-if [ $MOD -eq 0 ] ; then
-	FAILED=1
+	SYMS=( "bxt_dmc_ver1.bin" "kbl_dmc_ver1.bin" "skl_dmc_ver1.bin" "skl_guc_ver6.bin" )
+
+	for s in "${SYMS[@]}" ; do
+		if [ ! -h ${DIR_FIRMWARE}/$s ] ; then
+			ptest_fail
+		fi
+	done
+
+	FILES=( "bxt_dmc_ver1_07.bin" "kbl_dmc_ver1_01.bin" "skl_dmc_ver1_26.bin" "skl_guc_ver6_1.bin" )
+
+	for f in "${FILES[@]}" ; do
+		if [ ! -f ${DIR_FIRMWARE}/$f ] ; then
+			ptest_fail
+		fi
+	done
+}
+
+# Use dmidecode to check the system-manufacturer is 'National Instruments'.
+# If it is not, skip this test.
+function check_manufacturer () {
+	MAN=$(dmidecode --string system-manufacturer) \
+	|| echo "ERROR: dmicode call failed." 
+
+	if [ ! "${MAN}" == "National Instruments" ]; then
+		echo "REASON: Detected system manufacturer as '${MAN}'."
+		echo "REASON: We do not know if i915 should be tested."
+		ptest_skip
+	fi
+}
+
+# Passes iff the i915 module is loaded AND has >0 users. Fails otherwise.
+function check_module () {
+	MOD=$(lsmod | awk '/^i915/ {print $3}')
+	if [ -z "$MOD" ]; then
+		echo "i915 module not loaded."
+		ptest_fail
+		return
+	fi
+
+	echo -n "i915 module loaded."
+	if [ ${MOD:='0'} -gt 0 ]; then
+		ptest_pass
+	else
+		ptest_fail
+	fi
+	echo "${MOD} users."
+}
+
+check_arch
+check_manufacturer
+
+if [ $ptest_rc -ne 77 ]; then
+	check_files
+	check_module
 fi
 
-if [ $FAILED -eq 0 ] ; then
-	echo "PASS: `basename $0`"
-else
-	echo "FAIL: `basename $0`"
-fi
-
+ptest_report
+exit $ptest_rc
