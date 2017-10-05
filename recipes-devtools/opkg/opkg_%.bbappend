ni/meta-nilrt

commit cd8bd2642a9b36846ba5a41a17a528159d4707c6

opkg_%.bbappend: Add arch config for arm kernel packages
The Linux kernel, as it exists today and has for some time, will
always build for ARM as a softfp binary. Hence, NI device driver
modules for NXG ARM targets will need to be softfp.

Here, we introduce an Architecture to express the fact that the
contents of the package are softfp, but are kernel modules. This
allows for installation to hardfp targets without simply opening
those hardfp targets to install any ol' softfp package (e.g. a
softfp package from a regular LabVIEW 201x feed). This new Arch is
installed to NI Linux RT ARM NXG images, where such packages will
be needed.

Instead of attempting to plug into the incongruent default tunes used
(hardfloat builds don't have "arm"), pick something that will safely
be higher-priority than the basic "all", "any", and "noarch".

(discussed here https://nitalk.jiveon.com/blogs/blogobrad/2017/08/25/options-for-nxg-arm-packaging-feed-presence)

Signed-off-by: Brad Mouring <brad.mouring@ni.com>

recipes-devtools/opkg/opkg_%.bbappend
@@ -15,6 +15,10 @@ SRC_URI += " \
             file://0006-buffer-overrun-fix.patch \
            "
 
+SRC_URI_append_armv7a = " \
+            file://arm-kernel-arch.conf \
+"
+
 PACKAGECONFIG = "libsolv"
 
 RDEPENDS_${PN}-ptest += "bash"
@@ -22,4 +26,9 @@ RDEPENDS_${PN}-ptest += "bash"
 do_install_ptest() {
         cp ${WORKDIR}/test_feedserver.sh ${D}${PTEST_PATH}
         cp ${WORKDIR}/test_ni_pxi_install.sh ${D}${PTEST_PATH}
+}
+
+do_install_append_armv7a () {
+    install -d ${D}/opkg
+    install -m 0644 ${WORKDIR}/arm-kernel-arch.conf ${D}${sysconfdir}/opkg/
 }

recipes-devtools/opkg/files/arm-kernel-arch.conf
@@ -1,0 +1,1 @@
+arch cortexa9-vfpv3-kernel 12

commit d7593fb48477acf00bbe74128fe80b2ea1612506

opkg_%.bbappend: Fix directory creation
Introduced in cd8bd2642 ("opkg_%.bbappend: Add arch config for arm kernel packages")
the append for arm created a directory that is not installed by any
package, introducing a build-time warning at the same time as
not accomplishing the oroginal goal of being safe, ensuring that a
directory exists before installing a file to it.

Fixes: cd8bd2642 ("opkg_%.bbappend: Add arch config for arm kernel packages")
Signed-off-by: Brad Mouring <brad.mouring@ni.com>
Acked-by: Alex Stewart <alex.stewart@ni.com>
Acked-by: Ken Sharp <ken.sharp@ni.com>
Natinst-ReviewBoard-ID: 204441

recipes-devtools/opkg/opkg_%.bbappend
@@ -29,6 +29,6 @@ do_install_ptest() {
 }
 
 do_install_append_armv7a () {
-    install -d ${D}/opkg
+    install -d ${D}${sysconfdir}/opkg
     install -m 0644 ${WORKDIR}/arm-kernel-arch.conf ${D}${sysconfdir}/opkg/
 }