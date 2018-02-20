cc35abb91907b318ebd65a252b77e042db3dcd85
Alex Stewart <alex.stewart@ni.com>
2018-02-20 09:53:11 -0600

    hwclock-init: add ptests
    * Add hwclock-init-ptest subpackage to exercise the ``hwclock``
      functions required by /etc/init.d/hwclock.sh.
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>
    Acked-by: Adrian Ratiu <adrian.ratiu@ni.com>
    Acked-by: Brad Mouring <brad.mouring@ni.com>
    Acked-by: Ken Sharp <ken.sharp@ni.com>
    Natinst-CAR-ID: 677159
    Natinst-ReviewBoard-ID: 221937

---
+++ recipes-core/hwclock-init/files/test_hwclock.sh
@@ -0,0 +1,149 @@
+#! /bin/bash
+
+# Test the hwclock functionality needed by /etc/init.d/hwclock.sh
+# Namely: --hctosys, --systohc, & --show
+
+# Note that this script is destructive with respect to the state of the system
+# clock and rtc!
+
+source ./ptest-format.sh
+
+ptest_test=$(basename "$0" .sh)  # test_hwclock
+
+
+# SETUP TEMPDIR #
+#################
+
+declare -r HWCLOCK_TEMPDIR=$(mktemp -d)
+if [ -z "$HWCLOCK_TEMPDIR" ]; then
+	echo "ERROR: could not create tempdir" >&2 && \
+	exit 1
+fi
+
+cd $HWCLOCK_TEMPDIR
+
+# MISC FUNCTIONS #
+##################
+
+function nirtcfg_debug_output () {
+	test -n "$hwclock_args" && printf "<args>%s</args>\n" "$hwclock_args" >&2
+	test -n $hwclock_rc && printf "<return_code>%d</return_code>\n" >&2
+	test -n "$hwclock_stdout" && printf "<stdout>%s</stdout>\n" \
+	                                    "$hwclock_stdout" >&2
+	test -n "$hwclock_stderr" && printf "<stderr>%s</stderr>\n" \
+	                                    "$hwclock_stderr" >&2
+}
+
+function hwclock_store_output () {
+	hwclock_args="$@"
+	hwclock $@ 1>./hwclock_stdout 2>./hwclock_stderr
+
+	hwclock_rc=$?
+	hwclock_stdout=$(cat ./hwclock_stdout)
+	hwclock_stderr=$(cat ./hwclock_stderr)
+}
+
+function which_hwclock () {
+	skip_tests=false
+
+	which_hwclock=$(which hwclock)
+	if [ -z "$which_hwclock" ]; then
+		echo "ERROR: no hwclock implementaton present." >&2
+		return
+	else
+		printf "INFO: %s\n" "$(stat -c %N $which_hwclock)"
+	fi
+
+	echo "INFO:" $(hwclock --version)
+
+	output=$(hwclock --show 2>&1)
+	if [ $? -ne 0 ]; then
+		echo "INFO: No RTC detected, skipping tests."
+		skip_tests=true
+	fi
+}
+
+# SUBTESTS #
+############
+
+# Test that the hwclock can set the system clock
+# Expect return code 0, no stderr, and no stderr
+function test_hwclock_hctosys () {
+	ptest_pass
+	hwclock_store_output --hctosys
+
+	if [ $hwclock_rc -ne 0 -o \
+	     -n "$hwclock_stderr" -o \
+	     -n "$hwclock_stdout" ]; then
+		echo "ERROR: hwclock --hctosys returned error." >&2
+		hwclock_debug_output
+		ptest_fail
+	fi
+}
+
+# Test that hwclock_show returns code 0, no stderr, and some stdout
+function test_hwclock_show () {
+	ptest_pass
+	hwclock_store_output --show
+
+	if [ $hwclock_rc -ne 0 -o \
+	     -n "$hwclock_stderr" -o \
+	     -z "$hwclock_stdout" ]; then
+		echo "ERROR: hwclock --show returned error." >&2
+		hwclock_debug_output
+		ptest_fail
+	fi
+}
+
+# Test that the systemclock can set the hardware clock
+# Expect return code 0, no stderr, and no stderr
+function test_hwclock_systohc () {
+	ptest_pass
+	hwclock_store_output --systohc
+
+	if [ $hwclock_rc -ne 0 -o \
+	     -n "$hwclock_stderr" -o \
+	     -n "$hwclock_stdout" ]; then
+		echo "ERROR: hwclock --systohc returned error." >&2
+		hwclock_debug_output
+		ptest_fail
+	fi
+}
+
+# MAIN #
+########
+
+which_hwclock
+
+
+# --show
+ptest_change_subtest 1 "operation: show"
+if ! $skip_tests; then
+	test_hwclock_show
+else
+	ptest_skip
+fi
+ptest_report
+
+# --hctosys
+ptest_change_subtest 2 "operation: hctosys"
+if ! $skip_tests; then
+	test_hwclock_hctosys
+else
+	ptest_skip
+fi
+ptest_report
+
+# --systohc
+ptest_change_subtest 3 "operation: systohc"
+if ! $skip_tests; then
+	test_hwclock_systohc
+else
+	ptest_skip
+fi
+ptest_report
+
+# TEARDOWN #
+rm -r $HWCLOCK_TEMPDIR
+
+exit $ptest_rc
--- recipes-core/hwclock-init/hwclock-init_1.%.bbappend
+++ recipes-core/hwclock-init/hwclock-init_1.%.bbappend
@@ -1,3 +1,21 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+SRC_URI_append = "\
+    file://ptest-format.sh \
+    file://run-ptest \
+    file://test_hwclock.sh \
+"
+
+inherit ptest
+
 # Do not perform update-rc.d actions on the hwclock.sh initscript in this
 # package. We only wish to call hwclock.sh from /etc/init.d/bootmisc manually.
 INITSCRIPT_PACKAGES_remove = "${PN}"
+
+do_install_ptest() {
+    install -m 0644 ${WORKDIR}/ptest-format.sh ${D}${PTEST_PATH}
+    install -m 0755 ${WORKDIR}/run-ptest       ${D}${PTEST_PATH}
+    install -m 0755 ${WORKDIR}/test_hwclock.sh ${D}${PTEST_PATH}
+}
+
+RDEPENDS_${PN}-ptest += " bash "
