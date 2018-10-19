9099f6b6e24908ffa2d214d127b77e9872da9c12
Haris Okanovic <haris.okanovic@ni.com>
2018-10-19 13:22:59 -0500

    initscripts: hostname.sh: s/PXI-Unknown/Unknown, MAC address fallback
    The default hostname of NILRT systems is "NI-$MODEL-$SERIAL" where MODEL
    is the system's model name (such as "cRIO-9030") and SERIAL is a string
    like "A1B2C3D4". MODEL and SERIAL fall back to hardcoded values when
    they cannot be determined based on firmware/configuration.
    
    The following changes are made to better support generic machines/VMs:
     * MODEL falls back to "Unknown" instead of "PXI-Unknown"
     * SERIAL falls back to a MAC address when the system's
       serial number cannot be determined
    
    Signed-off-by: Haris Okanovic <haris.okanovic@ni.com>

--- recipes-core/initscripts/initscripts-1.0/hostname.sh
+++ recipes-core/initscripts/initscripts-1.0/hostname.sh
@@ -14,14 +14,36 @@ fi
 if [ $? -eq 0 ] && [ -n "$HOSTNAME" ]; then
     hostname "$HOSTNAME"
 else
-    # Generate a default hostname based on the serial# or MAC address
-    MODEL=$((/sbin/fw_printenv -n DeviceDesc 2> /dev/null || echo PXI-Unknown) | sed "s/^NI //;s/ Embedded Controller//")
+    # Generates a default hostname based on firmware DeviceDesc and serial# or MAC address
 
-    # Only use the last 8 characters of the serial number (after spaces are stripped)
-    SERIAL=$(/sbin/fw_printenv -n serial# 2>&1 | tr -d "\n " | tail -c 8)
+    # Grab model number from firmware
+    # Fixup Embedded Controller name
+    # Strip whitespace and only use the last 20 characters
+    MODEL=$((/sbin/fw_printenv -n DeviceDesc 2>/dev/null || echo "Unknown") | sed "s/^NI //;s/ Embedded Controller//" | tr -d "\n " | tail -c 20)
 
+    # Grab serial number from firmware
+    # Strip whitespace and only use the last 20 characters
+    SERIAL=$((/sbin/fw_printenv -n serial# 2>/dev/null) | tr -d "\n " | tail -c 20)
+
+    if [ -z "$SERIAL" ]; then
+        # Fallback to MAC address from network driver
+        # Prefer eth0
+        # Filer out lo adapter's MAC (all zeros)
+        SERIAL=$((cat /sys/class/net/eth0/address /sys/class/net/*/address 2>/dev/null) | grep -v '^00:00:00:00:00:00$' | head -1 | tr -d "\n: " | tail -c 20)
+    fi
+
+    if [ -z "$SERIAL" ]; then
+        # Unknown serial number
+        SERIAL="Unknown"
+    fi
+
+    # Set new hostname
     echo "NI-$MODEL-$SERIAL" | sed -r -e 's/[^a-zA-Z0-9]+/-/g' -e 's/^-//' | xargs hostname
 
-    # Set the newly generated hostname in /etc/hostname
+    # Persist new hostname in /etc/hostname file
     hostname > /etc/hostname
+
+    # Print new hostname to screen
+    echo -n "New system hostname: "
+    hostname
 fi
