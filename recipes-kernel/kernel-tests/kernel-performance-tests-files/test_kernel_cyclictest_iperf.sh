3186bc14044060aa1bd2f60ef8676f095c658f4e
Gratian Crisan <gratian.crisan@ni.com>
2020-08-13 17:28:55 -0500

    kernel-tests: Add kernel performance ptests (#23)
    kernel-tests: Add kernel performance ptests
    
    Add cyclictest performance tests with various loads: idle, hackbench
    scheduler load, fio based disk I/O load, and iperf based network load.
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    Acked-by: Alex Stewart <alex.stewart@ni.com>

---
+++ recipes-kernel/kernel-tests/kernel-performance-tests-files/test_kernel_cyclictest_iperf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source ./run-cyclictest
+
+# start background network load
+source ./iperf.cfg
+if [ -z "$IPERF_SERVER" ]; then
+    echo "Warning: iperf server not configured; skipping iperf based network load test."
+    echo "Edit `pwd`/iperf.cfg file to configure a server to connect to for this test."
+    echo "SKIP: test_kernel_cyclictest_iperf"
+    exit 77
+fi
+
+if [ ! -z "$IPERF_PORT" ]; then
+	iperf3 -c "$IPERF_SERVER" -p "$IPERF_PORT" -t 36000 --logfile "$LOG_DIR/iperf-`date +'%Y_%m_%d-%H_%M_%S'`.log" &
+    else
+	iperf3 -c "$IPERF_SERVER" -t 36000 --logfile "$LOG_DIR/iperf-`date +'%Y_%m_%d-%H_%M_%S'`.log" &
+fi
+
+# measure the system latency under network load
+run_cyclictest "iperf"
+
+# clean-up
+killall -INT iperf3 > /dev/null 2>&1
+
+exit $CYCLICTEST_RESULT
