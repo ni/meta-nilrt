5407c07dcdb657e29211b6f3132419ccba0ac6e3
James Minor <james.minor@ni.com>
2020-11-04 16:42:03 -0600

    niwatchdogpet: Ignore PXI targets
    Signed-off-by: James Minor <james.minor@ni.com>

--- recipes-ni/niwatchdogpet/files/niwatchdogpet.sh
+++ recipes-ni/niwatchdogpet/files/niwatchdogpet.sh
@@ -3,6 +3,11 @@
 
 PETTER=/usr/sbin/niwatchdogpet
 
+class="`/sbin/fw_printenv -n TargetClass`"
+
+# PXI targets use the built-in watchdog rather than CPLD
+[ "$class" = "PXI" ] && exit 0 
+
 # Make sure the petting tool is available.
 test -x "$PETTER" || exit 0
 
