ni/meta-nilrt

commit 46344db7da5f7200d780423cb55e5c6080e8cd7b

tzdata: Add postinst commands for localtime
Add postinst commands to point /etc/localtime to /etc/natinst/share/localtime.

Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>

recipes-extended/timezone/tzdata.bbappend
@@ -1,1 +1,6 @@
 INSTALL_TIMEZONE_FILE = "0"
+
+pkg_postinst_${PN}_append() {
+	# create symlink to shared default timezone settings (UTC)
+	ln -sf /etc/natinst/share/localtime $(D)/etc/localtime
+}

commit a7fca7d6bd6696f03307cb387f8869aa8c25e470

fixup! tzdata: Add postinst commands for localtime
Fix reference to the destination directory variable, D.
Fixes: 46344db

Signed-off-by: Brandon Streiff <brandon.streiff@ni.com>
Signed-off-by: Shruthi Ravichandran <shruthi.ravichandran@ni.com>

recipes-extended/timezone/tzdata.bbappend
@@ -2,5 +2,5 @@ INSTALL_TIMEZONE_FILE = "0"
 
 pkg_postinst_${PN}_append() {
 	# create symlink to shared default timezone settings (UTC)
-	ln -sf /etc/natinst/share/localtime $(D)/etc/localtime
+	ln -sf /etc/natinst/share/localtime $D/etc/localtime
 }