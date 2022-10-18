f368dd23c049899d6048624fa8da41c14407f129
Shruthi Ravichandran <shruthi.ravichandran@ni.com>
2022-10-17 18:57:48 -0700

    kernel_cyclictest_iperf: Fix indentation
    Fix indentation to match rest of the file.
    
    Signed-off-by: Shruthi Ravichandran <shruthi.ravichandran@ni.com>

--- recipes-kernel/kernel-tests/kernel-performance-tests-files/test_kernel_cyclictest_iperf.sh
+++ recipes-kernel/kernel-tests/kernel-performance-tests-files/test_kernel_cyclictest_iperf.sh
@@ -17,9 +17,9 @@ if [ ! -z "$IPERF_PORT" ]; then
         echo "SKIP: test_kernel_cyclictest_iperf"
         exit 77
     fi
-	iperf3 -c "$IPERF_SERVER" -p "$IPERF_PORT" -t 36000 --logfile "$LOG_DIR/iperf-`date +'%Y_%m_%d-%H_%M_%S'`.log" &
-    else
-	iperf3 -c "$IPERF_SERVER" -t 36000 --logfile "$LOG_DIR/iperf-`date +'%Y_%m_%d-%H_%M_%S'`.log" &
+    iperf3 -c "$IPERF_SERVER" -p "$IPERF_PORT" -t 36000 --logfile "$LOG_DIR/iperf-`date +'%Y_%m_%d-%H_%M_%S'`.log" &
+else
+    iperf3 -c "$IPERF_SERVER" -t 36000 --logfile "$LOG_DIR/iperf-`date +'%Y_%m_%d-%H_%M_%S'`.log" &
 fi
 
 # measure the system latency under network load
