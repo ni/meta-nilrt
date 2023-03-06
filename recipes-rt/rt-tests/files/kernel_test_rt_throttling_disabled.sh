959707d0defab90df6ec08eaf6b84da54651ac17
Gratian Crisan <gratian.crisan@ni.com>
2023-03-06 13:49:12 -0600

    rt-tests: Remove sub-test test_throttling_jitter
    The RT throttling ptest contains two sub-tests:
    
      - first the sysctl setting 'kernel.sched_rt_runtime_us' is checked for
        the expected -1 value which signifies that RT throttling is disabled in
        the kernel.
    
      - the second sub-test attempts to measure jitter as an indirect
        indication that RT throttling is disabled.
    
    However there are several issues with the second sub-test as indicated by
    the comments in 'test_throttling_jitter.c':
    
      - the test runs at the maximum RT priority available and pinned to
        CPU0. This will result in the target becoming unresponsive and result
        in various important kernel workloads timing out. This is especially
        true for CPU 0 which on NILRT services irq threads. It can result in
        subsequent tests failing, the root file system being re-mounted
        read-only, confusing error messages in the kernel log etc.
    
      - The threshold values used to detect a failure are arbitrary, vary
        between hardware variants, and have not been updated in a long while.
    
      - Due to the short duration of this test and lack of representative
        background workload this test is not a representative measure of
        real-time performance.
    
    Additionally, since this test was introduced we have added better coverage
    for long duration real-time kernel performance tests
    (i.e. meta-nilrt://recipes-kernel/kernel-tests/kernel-performance-tests.bb).
    These tests are expected to catch any RT throttling issues.
    
    Remove the problematic and redundant sub-test. Keep the sysctl setting
    check which has independent value.
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    (cherry picked from commit 6eab238a663d5f7491eb609b742990565fdcf309)
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- recipes-rt/rt-tests/files/kernel_test_rt_throttling_disabled.sh
+++ recipes-rt/rt-tests/files/kernel_test_rt_throttling_disabled.sh
@@ -10,23 +10,5 @@ else
 	ptest_pass
 fi
 ptest_report
-rc_first=$ptest_rc
 
-# now we do a jitter test to specifically verify that throttling is disabled
-LOG_FILE=$(dirname "$0")'/log.txt'
-ptest_change_subtest 2 "test throttling jitter"
-./test_throttling_jitter
-if [ $? -ne 0 ]; then
-	ptest_fail
-else
-	ptest_pass
-fi
-
-if [ ! -r "${LOG_FILE}" ]; then
-	echo "ERROR: Jitter log file ${LOG_FILE} is not readable."
-else
-	cat "${LOG_FILE}"
-fi
-ptest_report
-
-exit $rc_first
+exit $ptest_rc
