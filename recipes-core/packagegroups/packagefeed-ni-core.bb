ni/meta-nilrt

commit b8a8d05a007467bc3e710bfa0205b7fe0304e685

recipes-core/packagegroups: Remove packagegroup-ni-ptest
This packagegroup serves no purpose; tests have been and can continue to
be added to packagegroup-ni-desirable instead.

Signed-off-by: Mike Petersen <mike.petersen@ni.com>

recipes-core/packagegroups/packagefeed-ni-core.bb
@@ -12,7 +12,6 @@ RDEPENDS:${PN} = "\
 	packagegroup-ni-graphical \
 	packagegroup-ni-internal-deps \
 	packagegroup-ni-nohz-kernel \
-	packagegroup-ni-ptest \
 	packagegroup-ni-restoremode \
 	packagegroup-ni-runmode \
 	packagegroup-ni-safemode \

recipes-core/packagegroups/packagegroup-ni-desirable.bb
@@ -8,7 +8,6 @@ inherit packagegroup
 RDEPENDS:${PN} += "\
 	packagegroup-core-tools-debug \
 	packagegroup-ni-debug-kernel \
-	packagegroup-ni-ptest \
 	packagegroup-ni-selinux \
 "
 

recipes-core/packagegroups/packagegroup-ni-ptest.bb
@@ -1,21 +1,0 @@
-# (C) Copyright 2016,
-# National Instruments Corporation.
-# All rights reserved.
-
-SUMMARY = "Ptest packages for the NI Linux Realtime distribution"
-LICENSE = "MIT"
-
-PACKAGE_ARCH = "${MACHINE_ARCH}"
-
-inherit packagegroup
-
-RDEPENDS:${PN} = "ptest-runner"
-
-RDEPENDS:${PN} += "packagegroup-ni-ptest-smoke"
-
-# other supported ptest packages
-RDEPENDS:${PN}:append = "\
-"
-
-RDEPENDS:${PN}:append:nilrt-nxg = "\
-"

commit 0652842d6922726f6c40a9e32302332d96317b64

packagegroup-ni-core: Add packagegroup-ni-ptest-smoke
Removing the overall ptest package group removed the transitive
dependency from the core packagegroup to the ptest-smoke packagegroup,
preventing it from building and getting published to our feeds.

Fixes: b8a8d05a0074
Signed-off-by: Mike Petersen <mike.petersen@ni.com>

recipes-core/packagegroups/packagefeed-ni-core.bb
@@ -12,6 +12,7 @@ RDEPENDS:${PN} = "\
 	packagegroup-ni-graphical \
 	packagegroup-ni-internal-deps \
 	packagegroup-ni-nohz-kernel \
+	packagegroup-ni-ptest-smoke \
 	packagegroup-ni-restoremode \
 	packagegroup-ni-runmode \
 	packagegroup-ni-safemode \