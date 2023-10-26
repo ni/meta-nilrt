7b00e6a84c999ffc4cd1ca7a3078e74b8ea35b32
Dylan Turner <dylan.turner@ni.com>
2023-10-26 09:07:54 -0500

    kperf-cont-test: restart docker service before run
    Signed-off-by: Dylan Turner <dylan.turner@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_idle_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_idle_containerized.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Hack: Docker-CE must be restarted before containers can build. This is the first ran test
+/etc/init.d/docker.init restart
+
 PTEST_LOCATION=/usr/lib/kernel-containerized-performance-tests/ptest
 LOG_DIR="/var/local/ptest-results/kernel-containerized-performance-tests"
 
