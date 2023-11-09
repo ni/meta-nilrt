08b7077d5ae94a06043a9bb7409a4e868095bcdd
Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>
2023-11-09 11:52:18 -0600

    Revert "docker-functional-tests: hot fix docker restart"
    This reverts commit 725f76755b00107390747cb4322e95d35980b789.
    
    This commit is no longer necessary as a fix went into oe-core
    https://github.com/ni/openembedded-core/commit/c1bcd98d1d230971395dc966ddf032ebd7b6a79b.
    
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>

--- recipes-ni/docker-functional-tests/files/test_daemon.sh
+++ recipes-ni/docker-functional-tests/files/test_daemon.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Hack: Docker-CE must be restarted before containers can build. This is the first ran test
-/etc/init.d/docker.init restart
-
 echo "Running docker hello-world..."
 if ! docker run hello-world 2>&1; then
     echo "ERROR: Couldn't execute \`docker run hello-world\`"
