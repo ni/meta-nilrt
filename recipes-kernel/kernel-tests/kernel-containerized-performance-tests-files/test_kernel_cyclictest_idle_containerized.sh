0a4cf8bf9d2abe98a70952ea41502bbf2eb661ce
Gratian Crisan <gratian.crisan@ni.com>
2023-12-14 16:31:07 -0600

    kernel-containerized-performance-tests: re-factor the need for wrapper script
    Simplify container test logic
     - Remove need for intermediate log files to capture container output
       and remove associated wrapper script
     - Modify logging directory location in common.cfg to be applicable for
       all tests
     - Imporve sourcing of common.cfg
     - Improve exit code handling for clarity
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_idle_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_idle_containerized.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
-LOG_DIR="/var/local/ptest-results/kernel-containerized-performance-tests"
+source "$(dirname "$0")"/common.cfg
 
 # Run cyclictest
 echo "Running cyclictest in docker container..."
-RESULT=$(docker run --privileged --network=host \
-    -v ${LOG_DIR}:${LOG_DIR} -v ${PWD}:/ptests -v /home/admin:/home/admin \
-    -v /usr/share/fw_printenv:/usr/share/fw_printenv -v /sbin/fw_printenv:/sbin/fw_printenv \
-    -v /usr/share/nisysinfo:/usr/share/nisysinfo -v /dev:/dev \
-    -t cyclictest-container \
-    bash call_run_ct.sh "idle_containerized" \
-        | tr -d '\r' | tr -d '\n')
-
-# Make sure we print the PASS/FAIL message
-cat ${LOG_DIR}/run_cyclictest-idle_containerized.log
+docker run --privileged --network=host \
+       -v ${LOG_DIR}:${LOG_DIR} \
+       -v ${TEST_DIR}:${TEST_DIR} \
+       -v /home/admin:/home/admin \
+       -v /usr/share/fw_printenv:/usr/share/fw_printenv \
+       -v /sbin/fw_printenv:/sbin/fw_printenv \
+       -v /usr/share/nisysinfo:/usr/share/nisysinfo \
+       -v /dev:/dev \
+       -t cyclictest-container \
+       bash "${TEST_DIR}/run-cyclictest" "idle_containerized"
+RESULT=$?
 
 exit ${RESULT}
-
--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_iperf_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_iperf_containerized.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source "$(dirname "$0")"/common.cfg
 
 source /home/admin/.iperf.info
 if [ -z "$IPERF_SERVER" ]; then
@@ -24,16 +25,17 @@ LOAD_CONT=$(docker run -d --privileged \
 
 # Run cyclictest
 echo "Running cyclictest in docker container..."
-RESULT=$(docker run --privileged --network=host \
-    -v ${LOG_DIR}:${LOG_DIR} -v ${PWD}:/ptests -v /home/admin:/home/admin \
-    -v /usr/share/fw_printenv:/usr/share/fw_printenv -v /sbin/fw_printenv:/sbin/fw_printenv \
-    -v /usr/share/nisysinfo:/usr/share/nisysinfo -v /dev:/dev \
-    -t cyclictest-container \
-    bash call_run_ct.sh "iperf_containerized" \
-        | tr -d '\r' | tr -d '\n')
-
-# Make sure we print the PASS/FAIL message
-cat ${LOG_DIR}/run_cyclictest-iperf_containerized.log
+docker run --privileged --network=host \
+       -v ${LOG_DIR}:${LOG_DIR} \
+       -v ${TEST_DIR}:${TEST_DIR} \
+       -v /home/admin:/home/admin \
+       -v /usr/share/fw_printenv:/usr/share/fw_printenv \
+       -v /sbin/fw_printenv:/sbin/fw_printenv \
+       -v /usr/share/nisysinfo:/usr/share/nisysinfo \
+       -v /dev:/dev \
+       -t cyclictest-container \
+       bash "${TEST_DIR}/run-cyclictest" "iperf_containerized"
+RESULT=$?
 
 # Clean up the background container
 docker exec ${LOAD_CONT} \
