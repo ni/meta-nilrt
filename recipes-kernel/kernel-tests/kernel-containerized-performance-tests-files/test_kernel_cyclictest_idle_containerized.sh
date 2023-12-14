13a4bdc81d77b9aa41a1a760e1bfdb5615cbc856
Gratian Crisan <gratian.crisan@ni.com>
2023-12-14 09:26:40 -0600

    kernel-containerized-performance-tests: pull container build logic into central script
    Pull out common container build logic from each containerized
    performance test file into a single file and run it in run-ptest.
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    (cherry picked from commit 16be6c6df82b06c97250e5a1b28833826f293eba)
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_idle_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_idle_containerized.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-PTEST_LOCATION=/usr/lib/kernel-containerized-performance-tests/ptest
 LOG_DIR="/var/local/ptest-results/kernel-containerized-performance-tests"
-
-# Build the two containers
-if [ "$(docker images -q cyclictest-container:latest)" = "" ]; then
-    echo "Building cyclictest-container..."
-    DOCKER_BUILDKIT=1 \
-        docker build -t cyclictest-container --network=host ${PTEST_LOCATION}/cyclictest-container
-    if [ "$(docker images -q cyclictest-container:latest)" = "" ]; then
-        echo "Failed to build cyclictest-container"
-        exit 77
-    fi
-fi
 
 # Run cyclictest
 echo "Running cyclictest in docker container..."
 RESULT=$(docker run --privileged --network=host \
-    -v ${LOG_DIR}:${LOG_DIR} -v ${PTEST_LOCATION}:/ptests -v /home/admin:/home/admin \
+    -v ${LOG_DIR}:${LOG_DIR} -v ${PWD}:/ptests -v /home/admin:/home/admin \
     -v /usr/share/fw_printenv:/usr/share/fw_printenv -v /sbin/fw_printenv:/sbin/fw_printenv \
     -v /usr/share/nisysinfo:/usr/share/nisysinfo -v /dev:/dev \
     -t cyclictest-container \
