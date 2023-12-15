9fea47f3f45bd9bf92b1bf26967f05cb9effc1b1
Gratian Crisan <gratian.crisan@ni.com>
2023-12-15 15:59:28 -0600

    kernel-containerized-performance-tests: factor out docker calls
    - Single source cyclictest docker calls, params into a script
    - Remove superfluous syntax
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    (cherry picked from commit 20867641d49a240886cebe3833baf89c6c173b48)
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_iperf_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_iperf_containerized.sh
@@ -2,31 +2,19 @@
 source "$(dirname "$0")"/common.cfg
 
 # Start background network load
-echo "Starting iperf load..."
 LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -v ${LOG_DIR}:${LOG_DIR} \
 		   -v ${TEST_DIR}:${TEST_DIR} \
 		   -v /home/admin:/home/admin \
 		   -t parallel-container)
-docker exec ${LOAD_CONT} "${TEST_DIR}/iperf-load" "check" || exit $?
-docker exec -d ${LOAD_CONT} "${TEST_DIR}/iperf-load" "start"
+docker exec $LOAD_CONT "$TEST_DIR/iperf-load" "check" || exit $?
+docker exec -d $LOAD_CONT "$TEST_DIR/iperf-load" "start"
 
 # Run cyclictest
-echo "Running cyclictest in docker container..."
-docker run --privileged --network=host \
-       -v ${LOG_DIR}:${LOG_DIR} \
-       -v ${TEST_DIR}:${TEST_DIR} \
-       -v /home/admin:/home/admin \
-       -v /usr/share/fw_printenv:/usr/share/fw_printenv \
-       -v /sbin/fw_printenv:/sbin/fw_printenv \
-       -v /usr/share/nisysinfo:/usr/share/nisysinfo \
-       -v /dev:/dev \
-       -t cyclictest-container \
-       "${TEST_DIR}/run-cyclictest" "iperf_containerized"
-RESULT=$?
+"$TEST_DIR/run-docker-cyclictest" "iperf_containerized"
 
 # Clean up the background container
-docker exec ${LOAD_CONT} "${TEST_DIR}/iperf-load" "stop"
-docker stop ${LOAD_CONT}
+docker exec $LOAD_CONT "$TEST_DIR/iperf-load" "stop"
+docker stop $LOAD_CONT
 
-exit ${RESULT}
+exit $CYCLICTEST_RESULT
