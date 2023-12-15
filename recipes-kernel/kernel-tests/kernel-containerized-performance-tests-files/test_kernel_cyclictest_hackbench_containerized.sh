43ffcb3c08b707b663faa7a1a9f023d94097e29e
Gratian Crisan <gratian.crisan@ni.com>
2023-12-15 16:21:08 -0600

    kernel-containerized-performance-tests: run two cyclictest containers in parallel
    Modify hackbench test to run two cyclictest containers in parallel
    instead of one.
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>
    Signed-off-by: Chaitanya Vadrevu <chaitanya.vadrevu@ni.com>
    (cherry picked from commit 22d2e57830a0d5452cb40e3ec1b441af04277992)
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 source "$(dirname "$0")"/common.cfg
 
+CPUS=`nproc --all`
+if [ $CPUS -lt 2 ]; then
+	echo "ERROR: the containerized hackbench test requires a system with at least 2 CPUs"
+	echo "SKIP: test_kernel_hackbench_containerized"
+	exit 77
+fi
+
 # Start background scheduler load
 LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -v ${LOG_DIR}:${LOG_DIR} \
@@ -9,8 +16,20 @@ LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -t parallel-container)
 docker exec -d $LOAD_CONT "$TEST_DIR/hackbench-load" "start"
 
-# Run cyclictest
-"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized"
+# Run cyclictest containers on the last 2 CPUs on the system
+
+"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized_container_1" $((CPUS - 1)) &
+PID1=$!
+
+"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized_container_2" $((CPUS - 2))
+CYCLICTEST_RESULT_2=$?
+
+# Wait for first cyclictest to finish
+wait $PID1
+CYCLICTEST_RESULT_1=$?
+
+# Aggregate results from both cyclictests
+CYCLICTEST_RESULT=$(( $CYCLICTEST_RESULT_1 + $CYCLICTEST_RESULT_2 ))
 
 # Clean up the background container
 docker exec $LOAD_CONT "$TEST_DIR/hackbench-load" "stop"
