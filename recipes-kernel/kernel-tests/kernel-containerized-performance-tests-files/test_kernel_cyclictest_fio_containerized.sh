fab9bc2099a1fbb2ef2b143a38501ce4171fcbd0
Gratian Crisan <gratian.crisan@ni.com>
2023-12-15 15:59:28 -0600

    kernel-containerized-performance-tests: factor out docker calls
    - Single source cyclictest docker calls, params into a script
    - Remove superfluous syntax
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_fio_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_fio_containerized.sh
@@ -2,30 +2,18 @@
 source "$(dirname "$0")"/common.cfg
 
 # Start background disk I/O load
-echo "Starting fio load..."
 LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -v ${LOG_DIR}:${LOG_DIR} \
 		   -v ${TEST_DIR}:${TEST_DIR} \
 		   -v /home/admin:/home/admin \
 		   -t parallel-container)
-docker exec -d ${LOAD_CONT} "${TEST_DIR}/fio-load" "start"
+docker exec -d $LOAD_CONT "$TEST_DIR/fio-load" "start"
 
 # Run cyclictest
-echo "Running cyclictest in docker container..."
-docker run --privileged --network=host \
-       -v ${LOG_DIR}:${LOG_DIR} \
-       -v ${TEST_DIR}:${TEST_DIR} \
-       -v /home/admin:/home/admin \
-       -v /usr/share/fw_printenv:/usr/share/fw_printenv \
-       -v /sbin/fw_printenv:/sbin/fw_printenv \
-       -v /usr/share/nisysinfo:/usr/share/nisysinfo \
-       -v /dev:/dev \
-       -t cyclictest-container \
-       "${TEST_DIR}/run-cyclictest" "fio_containerized"
-RESULT=$?
+"$TEST_DIR/run-docker-cyclictest" "fio_containerized"
 
 # Clean up the background container
-docker exec ${LOAD_CONT} "${TEST_DIR}/fio-load" "stop"
-docker stop ${LOAD_CONT}
+docker exec $LOAD_CONT "$TEST_DIR/fio-load" "stop"
+docker stop $LOAD_CONT
 
-exit ${RESULT}
+exit $CYCLICTEST_RESULT
--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
@@ -2,31 +2,18 @@
 source "$(dirname "$0")"/common.cfg
 
 # Start background scheduler load
-echo "Starting hackbench load..."
 LOAD_CONT=$(docker run -d --privileged --network=host \
 		   -v ${LOG_DIR}:${LOG_DIR} \
 		   -v ${TEST_DIR}:${TEST_DIR} \
 		   -v /home/admin:/home/admin \
 		   -t parallel-container)
-docker exec -d ${LOAD_CONT} "${TEST_DIR}/hackbench-load" "start"
+docker exec -d $LOAD_CONT "$TEST_DIR/hackbench-load" "start"
 
 # Run cyclictest
-echo "Running cyclictest in docker container..."
-docker run --privileged --network=host \
-       -v ${LOG_DIR}:${LOG_DIR} \
-       -v ${TEST_DIR}:${TEST_DIR} \
-       -v /home/admin:/home/admin \
-       -v /usr/share/fw_printenv:/usr/share/fw_printenv \
-       -v /sbin/fw_printenv:/sbin/fw_printenv \
-       -v /usr/share/nisysinfo:/usr/share/nisysinfo \
-       -v /dev:/dev \
-       -t cyclictest-container \
-       "${TEST_DIR}/run-cyclictest" "hackbench_containerized"
-RESULT=$?
+"$TEST_DIR/run-docker-cyclictest" "hackbench_containerized"
 
 # Clean up the background container
-docker exec ${LOAD_CONT} "${TEST_DIR}/hackbench-load" "stop"
-docker stop ${LOAD_CONT}
+docker exec $LOAD_CONT "$TEST_DIR/hackbench-load" "stop"
+docker stop $LOAD_CONT
 
-exit ${RESULT}
-
+exit $CYCLICTEST_RESULT
