30a3fdcbfffa11edfa9ce7012c13f160fb3be232
Gratian Crisan <gratian.crisan@ni.com>
2023-12-15 14:58:40 -0600

    kernel-*-performance-tests: re-factor load wrapper scripts
    Simplify tetst logic for containerized and non-containerized tests
     - Remove unnecessary load wrapper scripts in containerized tests
     - Single source hackbench, fio, iperf start/stop logic in individual
       tests by moving into common scripts
     - Remove superfluous syntax
    
    Signed-off-by: Gratian Crisan <gratian.crisan@ni.com>

--- recipes-kernel/kernel-tests/files/parallel-container/run_hackbench.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /ptests
-source run-cyclictest
-hackbench -l 36000000 -g 10 \
-    2>/dev/null > "${LOG_DIR}/hackbench-`date +'%Y_%m_%d-%H_%M_%S\'`.log"
-
--- recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
+++ recipes-kernel/kernel-tests/kernel-containerized-performance-tests-files/test_kernel_cyclictest_hackbench_containerized.sh
@@ -3,8 +3,12 @@ source "$(dirname "$0")"/common.cfg
 
 # Start background scheduler load
 echo "Starting hackbench load..."
-LOAD_CONT=$(docker run -d --privileged -v ${PWD}:/ptests -t parallel-container \
-    bash run_hackbench.sh)
+LOAD_CONT=$(docker run -d --privileged --network=host \
+		   -v ${LOG_DIR}:${LOG_DIR} \
+		   -v ${TEST_DIR}:${TEST_DIR} \
+		   -v /home/admin:/home/admin \
+		   -t parallel-container)
+docker exec -d ${LOAD_CONT} "${TEST_DIR}/hackbench-load" "start"
 
 # Run cyclictest
 echo "Running cyclictest in docker container..."
@@ -17,12 +21,12 @@ docker run --privileged --network=host \
        -v /usr/share/nisysinfo:/usr/share/nisysinfo \
        -v /dev:/dev \
        -t cyclictest-container \
-       bash "${TEST_DIR}/run-cyclictest" "hackbench_containerized"
+       "${TEST_DIR}/run-cyclictest" "hackbench_containerized"
 RESULT=$?
 
 # Clean up the background container
-docker exec ${LOAD_CONT} \
-    bash -c "killall -INT hackbench > /dev/null 2>&1"
+docker exec ${LOAD_CONT} "${TEST_DIR}/hackbench-load" "stop"
+docker stop ${LOAD_CONT}
 
 exit ${RESULT}
 
