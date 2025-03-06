8e982e2ae30ed7b6b79f317cfec75ccecf255d57
Pratheeksha S N <pratheeksha.s.n@emerson.com>
2025-03-06 11:34:48 +0530

    kernel_dmesg_diff: Add command-line option for previous kernel dmesg logs
    Add the ability to specify a baseline log against which the current
    build's kernel_dmesg_diff test log is compared. This allows newer
    kernel dmesg outputs to be accepted without triggering test failures
    due to innocuous changes in the kernel dmesg output.
    
    This helps reduce noise in test failures caused by expected or minor
    changes in kernel messages while still catching unexpected regressions.
    
    Signed-off-by: Pratheeksha S N <pratheeksha.s.n@emerson.com>

--- recipes-kernel/kernel-tests/kernel-tests-files/test_kernel_dmesg_diff.sh
+++ recipes-kernel/kernel-tests/kernel-tests-files/test_kernel_dmesg_diff.sh
@@ -5,7 +5,11 @@ source $(dirname "$0")/ptest-format.sh
 ptest_change_test $(basename "$0" ".sh") "" "Diff dmesg log with with previous"
 
 source /home/admin/.mongodb.creds
-python3 kernel_dmesg_diff.py --server $MONGO_SERVER --user $MONGO_USER --password $MONGO_PASSWORD
+if [ -e /home/admin/.test.kernel_dmesg_diff.args ]; then
+   source /home/admin/.test.kernel_dmesg_diff.args
+fi
+
+python3 kernel_dmesg_diff.py --server $MONGO_SERVER --user $MONGO_USER --password $MONGO_PASSWORD "${KERNEL_DMESG_DIFF_TEST_EXTRA_ARGS[@]}"
 
 if [ $? -eq 0 ]; then
    ptest_pass
