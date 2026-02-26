51d66a7fc351801fb54bbe2737b76b53630bf6be
Alex Stewart <alex.stewart@emerson.com>
2026-02-26 12:07:28 -0500

    ni-systemformat:exit_codes.sh: make EXITCODES writable
    The EXITCODES variable is declared read-only, which means that bash will
    throw a warning if a script tries to import it twice, including through
    another import. We could defend against that case by checking if it has
    already been declared and then skipping redefinition. But it might be
    reasonable to change/overwrite the EXITCODES during script execution (if
    the script changes personality). So instead, just make it mutable.
    
    Signed-off-by: Alex Stewart <alex.stewart@emerson.com>

--- recipes-ni/ni-systemformat/ni-systemformat/src/lib/exit_codes.sh
+++ recipes-ni/ni-systemformat/ni-systemformat/src/lib/exit_codes.sh
@@ -1,5 +1,5 @@
 # List of returnable errors
-declare -Ar EXITCODES=(
+declare -A EXITCODES=(
 	[OK]=0                      # Success
 	[UNSPECIFIED]=1             # No reason specified
 	[UNKNOWN_ERROR]=2           # Unexpected exit code encountered
@@ -9,5 +9,6 @@ declare -Ar EXITCODES=(
 	[INVALID_ARCH]=6            # Running an unsupported processor architecture
 	[NO_KEY_BACKUP_DEVICE]=7    # Partition to store backup key could not be found; perhaps it's mislabeled or not plugged in
 	[IMPORT_ERROR]=8            # Error importing a script library
+	[BAD_ENVIRONMENT]=9         # The script was invoked in an unexpected environment, such as with missing dependencies or on an unsupported OS version
 )
 export EXITCODES
