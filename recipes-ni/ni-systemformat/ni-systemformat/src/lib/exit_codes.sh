b820a33e8a80dce83aa048cecdde113f27d0b746
Alex Stewart <alex.stewart@emerson.com>
2026-02-25 19:05:37 -0500

    ni-systemformat: break out functions into libs
    The ``nisystemformat`` main script is very large and difficult to
    follow. Break out its functionality into supporting shell libraries,
    which are sourced by the main file.
    
    Also revitalize the main (safemode) usage information in preparation for
    future extension.
    
    Signed-off-by: Alex Stewart <alex.stewart@emerson.com>

---
+++ recipes-ni/ni-systemformat/ni-systemformat/src/lib/exit_codes.sh
@@ -0,0 +1,13 @@
+# List of returnable errors
+declare -Ar EXITCODES=(
+	[OK]=0                      # Success
+	[UNSPECIFIED]=1             # No reason specified
+	[UNKNOWN_ERROR]=2           # Unexpected exit code encountered
+	[INVALID_ARGUMENT]=3        # An invalid combination of arguments was specified
+	[INVALID_FSTYPE]=4          # An invalid fstype was specified
+	[ILLTIMED]=5                # Called at an inappropriate time
+	[INVALID_ARCH]=6            # Running an unsupported processor architecture
+	[NO_KEY_BACKUP_DEVICE]=7    # Partition to store backup key could not be found; perhaps it's mislabeled or not plugged in
+	[IMPORT_ERROR]=8            # Error importing a script library
+)
+export EXITCODES
---
+++ recipes-ni/ni-systemformat/ni-systemformat/src/lib/util.sh
@@ -0,0 +1,60 @@
+# Utility functions for nisystemformat scripts.
+# These functions depend on the EXITCODES associative array, which should be
+# defined in the top level nisystemformat script before sourcing this file.
+#
+# Depends on exit_codes.sh for the EXITCODES associative array.
+
+
+COMMAND_RETRY_COUNT=30
+COMMAND_RETRY_SLEEP=1
+
+
+# Exit with an error message.
+function die() {
+	local exit_code="$1"  # exit code key from EXITCODES
+	local msg="${*:2}"  # error message text
+	echo -e "*** Error: ${BASH_SOURCE[1]}:${BASH_LINENO[1]}: $msg" >&2
+	exit ${EXITCODES[$exit_code]}
+}
+export -f die
+
+
+# Bash ERR trap handler: Prints an error message and exits with the
+# UNKNOWN_ERROR exit code.
+function handle_err() {
+	local src_path="$1"
+	local line_no="$2"
+	local func_name="$3"
+	local exit_code="$4"
+	echo "*** Error: $src_path:$line_no (fn=$func_name): Unexpected status code $exit_code" >&2
+	exit ${EXITCODES[UNKNOWN_ERROR]}
+}
+export -f handle_err
+
+
+# Retry loop for commands: Executes the specified command repeatedly
+# until it either exits with 0 or max number of retries are exhausted.
+function with_retry() {
+	local cmd="$@"  # command to execute (with args)
+	for attemptItr in `seq 1 "$COMMAND_RETRY_COUNT"`; do
+		if eval "$cmd"; then
+			return 0
+		fi
+
+		echo "$* failed with $? (attempt $attemptItr)" >&2
+		sleep "$COMMAND_RETRY_SLEEP"
+	done
+
+	echo "ERROR: $* failed ($COMMAND_RETRY_COUNT attempts)" >&2
+	return 1
+}
+export -f with_retry
+
+
+# Install the default error handler for the nisystemformat scripts, which will
+# print an error message and exit with the UNKNOWN_ERROR exit code whenever a
+# command exits with a non-zero status and is not otherwise handled.
+function install_default_err_handler() {
+	trap 'handle_err ${BASH_SOURCE} ${LINENO} ${FUNCNAME:-unknown} $?' ERR
+}
+export -f install_default_err_handler
