53e1b1b0ad58d5bb59a545d84329b3dac4c79b57
SandeepR-NI <Sandeep.R.Kumar@Emerson.com>
2026-07-02 21:48:10 +0530

    niauth-tests: add ptest for niauth packages
    - Add niauth-tests ptest recipe with 4 subtests:
      - pam-plugin-niauth is not installed
      - ni-auth-networkcontroller is not installed
      - libnss-niauth is not installed
      - niacctbase-sudo is not installed
    
    Signed-off-by: Sandeep R Kumar <Sandeep.R.Kumar@Emerson.com>

---
+++ recipes-ni/niauth-tests/files/test_niauth_packages.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Verifies that niauth-related packages and niacctbase-sudo are not installed.
+
+source "$(dirname "$0")/ptest-format.sh"
+
+ptest_test=$(basename "$0" ".sh")  # test_niauth_packages
+
+
+# TEST: pam-plugin-niauth is not installed
+function test_pam_plugin_niauth_not_installed() {
+    ptest_pass
+    if opkg list-installed | grep -q "^pam-plugin-niauth "; then
+        ptest_fail
+    fi
+}
+
+# TEST: ni-auth-networkcontroller is not installed
+function test_ni_auth_networkcontroller_not_installed() {
+    ptest_pass
+    if opkg list-installed | grep -q "^ni-auth-networkcontroller "; then
+        ptest_fail
+    fi
+}
+
+# TEST: libnss-niauth is not installed
+function test_libnss_niauth_not_installed() {
+    ptest_pass
+    if opkg list-installed | grep -q "^libnss-niauth "; then
+        ptest_fail
+    fi
+}
+
+
+ptest_change_subtest 1 "pam-plugin-niauth not installed"
+test_pam_plugin_niauth_not_installed
+ptest_report
+
+ptest_change_subtest 2 "ni-auth-networkcontroller not installed"
+test_ni_auth_networkcontroller_not_installed
+ptest_report
+
+ptest_change_subtest 3 "libnss-niauth not installed"
+test_libnss_niauth_not_installed
+ptest_report
+
+# TEST: niacctbase-sudo is not installed
+function test_niacctbase_sudo_not_installed() {
+    ptest_pass
+    if opkg list-installed | grep -q "^niacctbase-sudo "; then
+        ptest_fail
+    fi
+}
+
+ptest_change_subtest 4 "niacctbase-sudo not installed"
+test_niacctbase_sudo_not_installed
+ptest_report
---
+++ recipes-ni/niauth-tests/niauth-tests.bb
@@ -0,0 +1,33 @@
+SUMMARY = "Ptests confirming niauth-related packages are not installed"
+DESCRIPTION = "\
+Installs ptests that verify pam-plugin-niauth, ni-auth-networkcontroller, \
+libnss-niauth, and niacctbase-sudo packages are not present on the system."
+
+SECTION = "tests"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
+
+SRC_URI = "\
+    file://run-ptest \
+    file://ptest-format.sh \
+    file://test_niauth_packages.sh \
+"
+
+S = "${UNPACKDIR}"
+
+inherit ptest
+
+do_install_ptest() {
+    install -m 0755 ${S}/run-ptest              ${D}${PTEST_PATH}
+    install -m 0644 ${S}/ptest-format.sh        ${D}${PTEST_PATH}
+    install -m 0755 ${S}/test_niauth_packages.sh ${D}${PTEST_PATH}
+}
+
+ALLOW_EMPTY:${PN} = "1"
+
+# Only build the base (empty) and -ptest packages
+PACKAGES:remove = "${PN}-dev ${PN}-staticdev ${PN}-dbg"
+
+RDEPENDS:${PN}-ptest:append = " bash opkg"
